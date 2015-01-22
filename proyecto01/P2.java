d4aac984c6576197f8125391c95d153862fa8496
Carlos <carlos@carlos-PC.(none)>
2015-01-22 18:45:27 -0430

    Arreglado el bug de antes. Creo que ya funciona bien. Falta hacerle pruebas

--- proyecto01/P2.java
+++ proyecto01/P2.java
@@ -1,154 +1,168 @@
 import java.util.*; 
 
 public class P2 {
+    // public static void main(String[] args) {
+    // 	int nCases, nVertex, src, dst, numQueries, idEdge;
+    // 	int nComp = 1;
+    // 	String query;
+    // 	Vertex[] vertices;
+    // 	ArrayList<Edge> edges = new ArrayList<Edge>();
+    // 	Graph graph;
+    // 	Scanner scan = new Scanner(System.in);
+    // 	// Case[] cases;
+    // 	nCases = scan.nextInt();
+    // 	// cases = new Case[nCases];
+    // 	// for (int z = 0; z < nCases; z++) {
+    // 	    nVertex = scan.nextInt();
+    // 	    vertices = new Vertex[nVertex];
+    // 	    for (int i = 0; i < nVertex; i++) {
+    // 		vertices[i] = new Vertex(i+1);
+    // 	    }
+    // 	    for (int i = 1; i < nVertex; i++) {
+    // 		src = scan.nextInt();
+    // 		dst = scan.nextInt();
+    // 		edges.add(new Edge(src,dst));
+    // 		vertices[src-1].sons.add(vertices[dst-1]);
+    // 		vertices[dst-1].father = vertices[src-1];
+    // 	    }
+    // 	// for (int i = 0; i < nVertex; i++) {
+    // 	    vertices[0].numSons = Vertex.calcNumSons(vertices[0]);
+    // 	// Vertex.calcNumSons(vertices[0]);
+    // 	// }
+    // 	// for (int i = 0; i < nVertex; i++) {
+    // 	//     System.out.println("El padre del nodo #" + (i+1) + " es: " + vertices[i].father);
+    // 	//     for (int j = 0; j < vertices[i].sons.size(); j++) {
+    // 	// 	System.out.println(vertices[i].sons.get(j));
+    // 	//     }
+    // 	// }
+    // 	// cases[0] = new Case(nVertex, edges);
+    // 	// for (int i = 0; i < nVertex; i++) {
+    // 	//     System.out.println("El nodo #" + (i+1) + " tiene: " + vertices[i].numSons + " hijos");
+    // 	// }
+    // 	    graph = new Graph(vertices, edges);
+    // 	// System.out.println(graph);
+    // 	    numQueries = scan.nextInt();
+    // 	    for (int i = 0; i < numQueries; i++) {
+    // 		query = scan.next();
+    // 		if (query.equals("Q")) {
+    // 		    System.out.println("CALCULAR");
+    // 		    graph.calculateConnectedComponentSize();
+    // 		    System.out.println(graph.calculateDisconnectedOffices(nComp));
+    // 		} else {
+    // 		    nComp += 1;
+    // 		    idEdge = scan.nextInt();
+    // 		    System.out.println("REMOVER ARCO #" + idEdge);
+    // 		    graph.removeEdge(idEdge);
+    // 		}
+    // 	    }
+    // 	// }
+	    
+    // 	// scan.nextLine();
+    // 	// for (int i = 0; i < f; i++) {
+    // 	//     p = scan.nextLine();
+    // 	//     System.out.println(p);
+    // 	// }
+    // 	scan.close();
+    // 	System.out.println("¡Lo logré! ^*-*^");
+    // }
     public static void main(String[] args) {
-	int nCases, nVertex, src, dst, numQueries, idEdge;
-	String query;
-	Vertex[] vertices;
-	ArrayList<Edge> edges = new ArrayList<Edge>();
-	Graph graph;
-	Scanner scan = new Scanner(System.in);
-	// Case[] cases;
-	nCases = scan.nextInt();
-	// cases = new Case[nCases];
-	nVertex = scan.nextInt();
-	vertices = new Vertex[nVertex];
-	for (int i = 0; i < nVertex; i++) {
-	    vertices[i] = new Vertex(i+1);
-	}
-	for (int i = 1; i < nVertex; i++) {
-	    src = scan.nextInt();
-	    dst = scan.nextInt();
-	    edges.add(new Edge(src,dst));
-	    vertices[src-1].sons.add(vertices[dst-1]);
-	    vertices[dst-1].father = vertices[src-1];
-	}
-	// for (int i = 0; i < nVertex; i++) {
-	vertices[0].numSons = Vertex.calcNumSons(vertices[0]);
-	// Vertex.calcNumSons(vertices[0]);
-	// }
-	// for (int i = 0; i < nVertex; i++) {
-	//     System.out.println("El padre del nodo #" + (i+1) + " es: " + vertices[i].father);
-	//     for (int j = 0; j < vertices[i].sons.size(); j++) {
-	// 	System.out.println(vertices[i].sons.get(j));
-	//     }
-	// }
-	// cases[0] = new Case(nVertex, edges);
-	// for (int i = 0; i < nVertex; i++) {
-	//     System.out.println("El nodo #" + (i+1) + " tiene: " + vertices[i].numSons + " hijos");
-	// }
-	graph = new Graph(vertices, edges);
-	System.out.println(graph);
-	numQueries = scan.nextInt();
-	for (int i = 0; i < numQueries; i++) {
-	    query = scan.next();
-	    if (query.equals("Q")) {
-	    	System.out.println("CALCULAR");
-		graph.calculateConnectedComponentSize();
-		for (int k = 0; k < graph.vertices.length; k++) {
-		    System.out.println("El padre del nodo #" + (k+1) + " es: " + graph.vertices[k].father);
-		    System.out.println("Num hijos: " + graph.vertices[k].sons.size());
-		    for (int l = 0; l < graph.vertices[k].sons.size(); l++) {
-			System.out.println("Hijo #" + l + " :" + graph.vertices[k].sons.get(l));
-		    }
+	Case ca;
+	Case[] cases = Reader01.processInput();
+	for (int c = 0; c < cases.length; c++) {
+	    ca = cases[c];
+	    for (int i = 0; i < ca.queries.length; i++) {
+		
+		if (ca.queries[i].equals("Q")) {
+		    System.out.println("CALCULAR");
+		    ca.graph.calculateConnectedComponentSize();
+    		    System.out.println(ca.graph.calculateDisconnectedOffices());
+
+		} else {
+		    int idEdge = Integer.parseInt(ca.queries[i].split(" ")[1]);
+		    System.out.println("REMOVER ARCO #" + idEdge);
+		    ca.graph.nComponents += 1;
+		    ca.graph.removeEdge(idEdge);
 		}
 		
-	    } else {
-	    	idEdge = scan.nextInt();
-	    	System.out.println("REMOVER ARCO #" + idEdge);
-		graph.removeEdge(idEdge);
-		// for (int k = 0; k < graph.vertices.length; k++) {
-		//     System.out.println("El padre del nodo #" + (k+1) + " es: " + graph.vertices[k].father);
-		//     System.out.println("Num hijos: " + graph.vertices[k].sons.size());
-		//     for (int l = 0; l < graph.vertices[k].sons.size(); l++) {
-		// 	System.out.println("Hijo #" + l + " :" + graph.vertices[k].sons.get(l));
-		//     }
-		// }
 	    }
+    		// if (query.equals("Q")) {
+    		//     System.out.println("CALCULAR");
+    		//     graph.calculateConnectedComponentSize();
+    		//     System.out.println(graph.calculateDisconnectedOffices(nComp));
+    		// } else {
+    		//     nComp += 1;
+    		//     idEdge = scan.nextInt();
+    		//     System.out.println("REMOVER ARCO #" + idEdge);
+    		//     graph.removeEdge(idEdge);
+    		// }
+
 	}
-	    
-	// scan.nextLine();
-	// for (int i = 0; i < f; i++) {
-	//     p = scan.nextLine();
-	//     System.out.println(p);
-	// }
-	scan.close();
-	System.out.println("¡Lo logré! ^*-*^");
-    }
 	
     // public static calculate(Vertex vertex) {
     // }
+    }
 }
-
-// class Reader01 {    
-//     public static Case[] processInput() {
-// 	int n, src, dst, act;
-// 	// String ;
-// 	Scanner scan = new Scanner(System.in);
-// 	nCases = scan.nextInt();
-// 	// For each case, get data
-// 	for (int i = 0; i < nCases; i++) {
-// 	    n = scan.nextInt();
-// 	    System.out.println("Cantidad de nodos: " + n);
-// 	    for (int j = 0; j < n-1; j++) {
-// 		src = scan.nextInt();
-// 		dst = scan.nextInt();
-// 		System.out.println("ARCO #" + j + " (" + src + "," + dst + ")");
-// 	    }
-// 	    act = scan.nextInt();
-// 	    for (int j = 0; j < act-1; j++) {
-// 		p = scan.nextLine();
-// 		System.out.println(p);
-// 	    }
-// 	}
-	
-// 	scan.close();
-// 	return cases;
-//     }
-// }
 
 // Contains the given data for each case
 class Case {
-    public int numVertex;
-    public int[] fathers; 
-    public int[] numChildren;
-    public ArrayList<Edge> edges;
-    
-    public Case(int numVertex, ArrayList<Edge> edges) {
-	this.numVertex = numVertex;
-	System.out.println("NUMVERTEX: " + numVertex);
-	fathers = new int[numVertex]; 
-	// colors = new int[numVertex]; 
-	numChildren = new int[numVertex];
-	this.edges = edges;
-	for (int i = 0; i < numVertex; i++) {
-	    // this.colors[i] = 1;
-	    this.numChildren[i] = 0;
-	}
-	this.fathers[0] = 0; 
-	for (int i = 0; i < numVertex - 1; i++) {
-	    Edge edge = this.edges.get(i);
-	    this.fathers[edge.dst - 1] = edge.src - 1;
-	    this.numChildren[edge.src-1] += 1;
-	    // while (this.fathers[src-1] != ) {
-		
-		
-		   
-	    // }	    
-	}
+    Graph graph;
+    String[] queries;
+
+    public Case(Vertex[] vertices, ArrayList<Edge> edges, String[] queries) {
+	this.graph = new Graph(vertices, edges);
+	this.queries = queries;
     }
 }
+
+class Reader01 {    
+    public static Case[] processInput() {
+	int nCases, nVertex, src, dst, numQueries, idEdge;
+	int nComp = 1;
+	String q;
+	String[] queries;
+	Vertex[] vertices;
+	ArrayList<Edge> edges = new ArrayList<Edge>();
+	Scanner scan = new Scanner(System.in);
+	Case[] cases;
+	nCases = scan.nextInt();
+	cases = new Case[nCases];
+	for (int i = 0; i < nCases; i++) {
+	    nVertex = scan.nextInt();
+	    vertices = new Vertex[nVertex];
+	    for (int j = 0; j < nVertex; j++) {
+		vertices[j] = new Vertex(j+1);
+	    }
+	    for (int j = 1; j < nVertex; j++) {
+		src = scan.nextInt();
+		dst = scan.nextInt();
+		edges.add(new Edge(j, src,dst));
+		vertices[src-1].sons.add(vertices[dst-1]);
+		vertices[dst-1].father = vertices[src-1];
+	    }
+	    vertices[0].numSons = Vertex.calcNumSons(vertices[0]);
+	    numQueries = scan.nextInt();
+	    scan.nextLine();
+	    queries = new String[numQueries];
+    	    for (int j = 0; j < numQueries; j++) {
+    		queries[j] = scan.nextLine();
+    	    }
+	    cases[i] = new Case(vertices, edges, queries);
+	}
+	scan.close();
+	return cases;
+    }
+}
+
 
 class Graph {
     public Vertex[] vertices;
     public ArrayList<Edge> edges;
+    public int nComponents;
 
     public Graph(Vertex[] vertices, ArrayList<Edge> edges) {
 	this.vertices = vertices;
-	for (int i = 0; i < vertices.length; i++) {
-	    System.out.println(this.vertices[i].numSons);
-	}
 	this.edges = edges;
+	nComponents = 1;
     }
     
     public String toString() {
@@ -164,7 +178,12 @@ class Graph {
 
     public void removeEdge(int idEdge) {
 	int i = 0;
-	Edge edge = this.edges.get(idEdge-1);
+	Edge edge;
+	while (i < edges.size() && edges.get(i).id != idEdge) {
+	    i += 1;
+	}
+	edge = edges.get(i);
+	i = 0;
 	Vertex vertex = vertices[edge.src -1];
 	while (vertex.sons.get(i).id != edge.dst) {
 	    i += 1;
@@ -179,10 +198,36 @@ class Graph {
 	Vertex vertex;
 	for (int i = 0; i < size; i++) {
 	    vertex = vertices[i];
-	    if (vertex.father.id == (i+1)) {
+	    vertex.numSons = 1;
+	    if (vertex.father.id == vertex.id) {
 		vertex.numSons = Vertex.calcNumSons(vertex);
 	    }
 	}	
+    }
+
+    public int calculateDisconnectedOffices() {
+	int i = 0;
+	int j = 0;
+	int total = 0;
+	int[] sizeComp = new int[nComponents];
+	if (nComponents != 1) {
+	    while (j < nComponents && i < vertices.length) {
+	    	if (vertices[i].id == (i + 1)) {
+	    	    sizeComp[j] = vertices[i].numSons;
+	    	    j += 1;
+	    	}
+		i += 1;
+	    }
+		
+	    for (i = 0; i < nComponents; i++) {
+		for (j = 0; j < nComponents; j++) {
+		    if (i != j) {
+			total += sizeComp[i] * sizeComp[j];
+		    }
+		}
+	    }
+	}
+	return (total/2);
     }
 }
 
@@ -203,7 +248,6 @@ class Vertex {
     }
 
     public static int calcNumSons(Vertex vertex) {
-	// int total = 1;
 	int size = vertex.sons.size();
 	Vertex v;
 	if (vertex.sons != null) {	    
@@ -215,31 +259,13 @@ class Vertex {
 	}
 	return vertex.numSons;
     }
-
-    // public static void removeEdge(int idEdge, ArrayList<Edge> edges) {
-    // 	int i = 0;
-    // 	int size;
-    // 	Edge edge = edges.get(idEdge);
-    // 	Vertex son;
-    // 	Vertex vertex = vertices[edge.src-1];
-    // 	size = vertex.sons.length;
-	
-    // 	while (i < size) {
-    // 	    son = vertex.sons.get(i);
-    // 	    if (son.id == edge.src) {
-    // 		vertex.remove(son);
-    // 		break;
-    // 	    }
-    // 	    i +=1;
-    // 	}
-    // }
-
 }
 
 class Edge {
-    public int src, dst;
+    public int id, src, dst;
     
-    public Edge(int src, int dst) {
+    public Edge(int id, int src, int dst) {
+	this.id = id;
 	this.src = src;
 	this.dst = dst;
     }
