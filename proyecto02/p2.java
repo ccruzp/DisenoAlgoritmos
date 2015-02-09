a6a7a94a0a9afb4391f294ecd41b01dcb44190a3
Carlos <carlos@carlos-PC.(none)>
2015-02-09 12:24:57 -0430

    p2.java es el main

---
+++ proyecto02/p2.java
@@ -0,0 +1,23 @@
+/**
+ * Implements a tester for the colored graph solution class
+ */
+public class p2 {
+
+    public static void main(String[] args) {
+
+        // Verify arguments
+        if (args.length != 1) {
+            System.out.println("Falta el argumento archivo.");
+            System.exit(1);
+        }
+
+        // Read graph
+        ColoredGraph graph = DimacsReader.processInput(args[0]);
+
+        // Solve Graph
+        graph.solveColoring();
+
+        // Return Chromatic Number
+        System.out.println(graph.getChromaticNumber());
+    }
+}
